1fe1158a084199abc0c985d946d351c060bd3204
DecoudJuan <decoudjuanmanuel1@gmail.com>
2025-08-13 22:52:54 -0300

    feat: AST Printer y tests

---
+++ src/main/kotlin/printer/AstPrinter.kt
@@ -0,0 +1,33 @@
+package printer
+
+import common.AbstractSyntaxTree
+import common.BinOp
+import common.Num
+import common.UnaryOp
+
+class AstPrinter {
+    fun print(node: AbstractSyntaxTree, indent: String = ""): String {
+        if (node is Num) {
+            return "$indent${node.value}"
+        }
+        if (node is BinOp) {
+            val left = print(node.left, "$indent  ")
+            val right = print(node.right, "$indent  ")
+            return buildString {
+                append("$indent(${node.op.type}\n")
+                append(left).append("\n")
+                append(right).append("\n")
+                append(indent).append(")")
+            }
+        }
+        if (node is UnaryOp) {
+            val expr = print(node.expr, "$indent  ")
+            return buildString {
+                append("$indent(${node.op.type}\n")
+                append(expr).append("\n")
+                append(indent).append(")")
+            }
+        }
+        error("Nodo desconocido: ${node::class.simpleName}")
+    }
+}
---
+++ src/test/kotlin/AstPrinterTest.kt
@@ -0,0 +1,166 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import parser.Parser
+import scanning.Tokenizer
+import printer.AstPrinter
+
+class AstPrinterTest {
+
+    private fun printAst(src: String): String {
+        val tokenizer = Tokenizer(src)
+        val parser = Parser(tokenizer)
+        val ast = parser.parseProgram()
+        return AstPrinter().print(ast)
+    }
+
+    @Test
+    fun `should print single number without extra wrapping`() {
+        val expected = """
+            10
+        """.trimIndent()
+        assertEquals(expected, printAst("10"))
+    }
+
+    @Test
+    fun `should print simple addition as parenthesized PLUS with two indented numbers`() {
+        val expected = """
+            (ADD
+              3
+              5
+            )
+        """.trimIndent()
+        assertEquals(expected, printAst("3 + 5"))
+    }
+
+    @Test
+    fun `should print unary minus as parenthesized MINUS with one indented expr`() {
+        val expected = """
+            (SUB
+              5
+            )
+        """.trimIndent()
+        assertEquals(expected, printAst("-5"))
+    }
+
+    @Test
+    fun `should print nested binary operations respecting precedence`() {
+        // 3 + 5 * 2  => PLUS(3, MUL(5, 2))
+        val expected = """
+            (ADD
+              3
+              (MUL
+                5
+                2
+              )
+            )
+        """.trimIndent()
+        assertEquals(expected, printAst("3 + 5 * 2"))
+    }
+
+    @Test
+    fun `should print parentheses affecting precedence`() {
+        // (3 + 5) * 2  => MUL(PLUS(3,5), 2)
+        val expected = """
+            (MUL
+              (ADD
+                3
+                5
+              )
+              2
+            )
+        """.trimIndent()
+        assertEquals(expected, printAst("(3 + 5) * 2"))
+    }
+
+    @Test
+    fun `should print nested parentheses`() {
+        // 7 + 3 * (10 / (12 / (3 + 1) - 1))
+        val expected = """
+            (ADD
+              7
+              (MUL
+                3
+                (DIV
+                  10
+                  (SUB
+                    (DIV
+                      12
+                      (ADD
+                        3
+                        1
+                      )
+                    )
+                    1
+                  )
+                )
+              )
+            )
+        """.trimIndent()
+        assertEquals(expected, printAst("7 + 3 * (10 / (12 / (3 + 1) - 1))"))
+    }
+
+    @Test
+    fun `should print expression with surrounding spaces identically to no spaces`() {
+        val expected = """
+            (SUB
+              (MUL
+                (ADD
+                  2
+                  3
+                )
+                4
+              )
+              (DIV
+                10
+                5
+              )
+            )
+        """.trimIndent()
+        val withSpaces = " (  (2 + 3) * 4 ) - ( 10 / 5 ) "
+        val noSpaces  = "((2+3)*4)-(10/5)"
+        assertEquals(expected, printAst(withSpaces))
+        assertEquals(expected, printAst(noSpaces))
+    }
+
+    @Test
+    fun `should print unary applied to parenthesized expression`() {
+        // -(3 + 2) => MINUS(PLUS(3,2))
+        val expected = """
+            (SUB
+              (ADD
+                3
+                2
+              )
+            )
+        """.trimIndent()
+        assertEquals(expected, printAst("-(3 + 2)"))
+    }
+
+    @Test
+    fun `should print combined operations with all operators`() {
+        // 15 * 10 + 10 / 5 - 2 + 5 / 5
+        // => PLUS( MINUS( PLUS( MUL(15,10), DIV(10,5) ), 2 ), DIV(5,5) )
+        val expected = """
+            (ADD
+              (SUB
+                (ADD
+                  (MUL
+                    15
+                    10
+                  )
+                  (DIV
+                    10
+                    5
+                  )
+                )
+                2
+              )
+              (DIV
+                5
+                5
+              )
+            )
+        """.trimIndent()
+        assertEquals(expected, printAst("15 * 10 + 10 / 5 - 2 + 5 / 5"))
+    }
+}
