65af3f0d1b16570d0b68479c0bb5b9beb225c386
Julian Ritondale <jritondale@mail.austral.edu.ar>
2025-10-28 13:54:24 -0300

    feat: add interpreter visitor table and registry

---
+++ printscript-interpreter/src/main/kotlin/interpreter/internal/table/VisitorTableRegistry.kt
@@ -0,0 +1,20 @@
+package interpreter.internal.table
+
+import model.visitor.context.ContextVisitorTable
+import type.option.Option
+
+object VisitorTableRegistry {
+    private val tables: Map<String, Lazy<ContextVisitorTable>> = mapOf(
+        "1.0" to lazy { PrintScriptV10 },
+        "1.1" to lazy { PrintScriptV11 },
+    )
+
+    fun get(version: String): Option<ContextVisitorTable> {
+        val lazy = tables[version.lowercase()]
+        return if (lazy != null) {
+            Option.Some(lazy.value)
+        } else {
+            Option.None
+        }
+    }
+}
