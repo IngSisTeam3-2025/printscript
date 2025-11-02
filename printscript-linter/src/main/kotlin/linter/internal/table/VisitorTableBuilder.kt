f89897957fd67dbfe304a207ec9963ddf2ee0703
Juan Decoud <decoudjuanmanuel1@gmail.com>
2025-11-02 14:14:23 -0300

    Visitors and tables

--- printscript-linter/src/main/kotlin/linter/internal/table/VisitorTableBuilder.kt
+++ printscript-linter/src/main/kotlin/linter/internal/table/VisitorTableBuilder.kt
@@ -1,8 +1,11 @@
 package linter.internal.table
 
+import linter.internal.model.error.ConfigurationError
+import model.diagnostic.Diagnostic
 import model.rule.Rule
 import model.visitor.Visitor
 import model.visitor.VisitorTable
+import type.outcome.Outcome
 
 internal interface VisitorTableBuilder {
 
@@ -10,15 +13,21 @@ internal interface VisitorTableBuilder {
 
     val factories: Map<String, VisitorFactory>
 
-    fun build(rules: Collection<Rule>): VisitorTable {
+    @Suppress("TooGenericExceptionCaught", "SwallowedException")
+    fun build(rules: Collection<Rule>): Outcome<VisitorTable, Diagnostic> {
         val visitors = mutableListOf<Visitor>()
 
         for (rule in rules) {
-            val factory = factories[rule.signature]
-            if (factory != null) {
+            val factory = factories[rule.signature] ?: continue
+            try {
                 visitors.add(factory.create(rule))
+            } catch (e: ClassCastException) {
+                val type = rule.value.type()
+                val message = "Rule '${rule.signature}' value has invalid type '$type'"
+                return Outcome.Error(ConfigurationError(message))
             }
         }
-        return DefaultVisitorTable(visitors)
+
+        return Outcome.Ok(DefaultVisitorTable(visitors))
     }
 }
